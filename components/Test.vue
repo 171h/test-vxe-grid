4a5afacbb0c308cbc1e70726cec78137db2e3df0
H171 <1091088708@qq.com>
2023-05-02 20:10:40 +0800

    feat: many update

--- components/Test.vue
+++ components/Test.vue
@@ -1,8 +1,20 @@
 <script setup lang="ts">
 import { defineComponent, ref, reactive } from "vue";
-import { VxeGridProps, VxeTableInstance, VXETable, VxeToolbarInstance } from "vxe-table";
+import { VxeGridProps, VxeTableInstance, VXETable, VxeToolbarInstance, VxeTablePropTypes, VxeTableEvents } from "vxe-table";
 import * as _ from "lodash-es";
 import { Logger } from "@171h/log";
+
+/**
+ * 测试功能：
+ * 1. 数据的增删改查
+ * 2. 数据的底部添加空行
+ * 3. 数据的底部空行的滚动加载与删除
+ * 4. 编辑渲染器的 v-model.lazy 的使用, 待解决阻止 textarea enter 换行问题 【基本解决】
+ * 5. 数据的扁平化导出功能
+ * 6. cell 的上下左右边距过宽的问题【已解决】
+ * 7. 解决 tree 树形表格的单元格 edit 状态无法撑满 td 的问题【已解决】
+ *
+ */
 
 const logger = new Logger("test.vue");
 
@@ -51,27 +63,6 @@ const demo1 = reactive({
 
 const data2 = ref(data);
 
-// 修改数据===========================================>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-async function add() {
-  const id = data[data.length - 1].id + 1;
-  // const item = { id, name: `Test${id}`, role: "addfsdf", sex: "Man", age: 28, address: "test abc" };
-  // data2.value.push(item);
-  // logger.debug('before nextTick',item);
-  // await nextTick();
-  // logger.debug('after nextTick',item);
-}
-
-function remove() {
-  data2.value.pop();
-}
-
-function modify(id: number) {
-  const item = data2.value.find((item) => item.id === id);
-  if (item) {
-    item.name = Math.random().toString();
-  }
-}
-
 // 工具栏绑定===========================================>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 const xGrid = ref<VxeTableInstance>();
 const xToolbar = ref<VxeToolbarInstance>();
@@ -83,15 +74,23 @@ nextTick(() => {
   }
 });
 
+// ===========================================>>>>>>>>>>>>>>>>>>>>>>>>>>> gridOptions
 const gridOptions = reactive<VxeGridProps<RowVO>>({
   border: true,
   columns: [
     { type: "checkbox", width: 50, showOverflow: true, align: "center" },
     { type: "seq", width: 100 },
-    { field: "name", title: "Name", slots: { default: "name" }, treeNode: true },
-    { field: "size", title: "Size", showHeaderOverflow: true },
-    { field: "date", title: "Date", showOverflow: true },
-    {title: "操作", slots: { default: 'action' }, width: 200}
+    { field: "name", title: "Name", slots: { edit: "text_edit" }, treeNode: true, editRender: { enabled: true } },
+    { field: "type", title: "Type", slots: { edit: "text_edit" }, showOverflow: true, editRender: { enabled: true } },
+    {
+      field: "size",
+      title: "Size",
+      slots: { edit: "text_edit" },
+      showHeaderOverflow: true,
+      editRender: { enabled: true },
+    },
+    { field: "date", title: "Date", slots: { edit: "text_edit" }, showOverflow: true, editRender: { enabled: true } },
+    { title: "操作", slots: { default: "action" }, width: 400, editRender: { enabled: true } },
   ],
   treeConfig: {
     transform: true,
@@ -109,6 +108,11 @@ const gridOptions = reactive<VxeGridProps<RowVO>>({
     highlight: true,
   },
   data: data2.value,
+  editConfig: {
+    trigger: "click",
+    mode: "cell",
+    showStatus: true,
+  },
 });
 
 // 读取文件===========================================>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -190,11 +194,24 @@ const printEvent1 = () => {
 
 // 增删改查===========================================>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 // 增加行
+const insertRowEnd = async (num: number) => {
+  const $table = xGrid.value!;
+  for (let i = 0; i < num; i++) {
+    const date = new Date();
+    const record = {
+      name: `新数据 ${i + 1}`,
+      id: Date.now(),
+      parentId: null,
+      date: `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`,
+    };
+    const { row: newRow } = await $table.insertAt(record, -1);
+  }
+};
+
 const insertRow = async (currRow: any, locat: string) => {
   const $table = xGrid.value!;
   const date = new Date();
 
-  
   // 如果 null 则插入到目标节点顶部
   // 如果 -1 则插入到目标节点底部
   // 如果 row 则有插入到效的目标节点该行的位置
@@ -241,16 +258,47 @@ const removeRow = async (row: any) => {
   const $table = xGrid.value!;
   await $table.remove(row);
 };
+
+// 禁用 enter
+// function checkEnter(e: KeyboardEvent) {
+//   var et = e || window.event;
+//   var keycode = et.charCode || et.keyCode;
+//   if (keycode == 13) {
+//     if (window.event) {
+//       window.event.returnValue = false;
+//     } else {
+//       e.preventDefault(); //for firefox
+//     }
+//   }
+// }
+
+/* 解决 tree 树形表格的单元格 edit 状态无法撑满 td 的问题 */
+const selectCurrent = reactive({
+  selectRow: null as any,
+  selectColumn: null as any,
+});
+const cellClassName: VxeTablePropTypes.CellClassName = ({ row, column }) => {
+  if (row === selectCurrent.selectRow && column === selectCurrent.selectColumn) {
+    logger.debug("cellClassName", row, column);
+    return "no-indent";
+  }
+  return null;
+};
+const cellClickEvent: VxeTableEvents.CellClick = ({ row, column }) => {
+  selectCurrent.selectRow = row;
+  selectCurrent.selectColumn = column;
+};
 </script>
 
 <template>
   <div>
     <h1>Test</h1>
-    <vxe-button @click="add">add</vxe-button>
+    <!-- <vxe-button @click="add">add</vxe-button>
     <vxe-button @click="remove">remove</vxe-button>
-    <vxe-button @click="modify(10001)">modify</vxe-button>
+    <vxe-button @click="modify(10001)">modify</vxe-button> -->
     <vxe-button @click="clickEvent1">读取文件</vxe-button>
     <vxe-button @click="printEvent1">打印</vxe-button>
+    <vxe-button @click="insertRowEnd(10)">在尾部插入空白行</vxe-button>
   </div>
   <div>
     <i class="vxe-icon-alipay roll"></i>
@@ -277,9 +325,25 @@ const removeRow = async (row: any) => {
       <vxe-button type="text" icon="vxe-icon-funnel" class="tool-btn" @click=""></vxe-button>
     </template>
   </vxe-toolbar>
-  <vxe-grid ref="xGrid" v-bind="gridOptions">
-    <template #name="{ row }">
-      <span>自定义插槽模板 {{ row.name }}</span>
+  <vxe-grid ref="xGrid" :cell-class-name="cellClassName" @cell-click="cellClickEvent" v-bind="gridOptions">
+    <template #text="{ row }">
+      <span>{{ row.name }}</span>
+    </template>
+    <template #text_edit="{ row }">
+      <!-- <vxe-input v-model="row.name"></vxe-input> -->
+      <!-- 解决 vxe-inpu 无法 v-mode.lazy 的问题-->
+      <!-- <input class="vxe-input--inner" v-model.lazy="row.name" /> -->
+      <textarea
+        class="vxe-input--inner"
+        style="overflow: visible; resize: none; border-radius: 0; border: none; padding: 0;"
+        :cols="5"
+        :spellcheck="false"
+        @keyup.enter.preventDefault()
+        @keypress.enter.preventDefault()
+        @keydown.enter.preventDefault()
+        wrap="hard"
+        v-model.lazy="row.name"
+      ></textarea>
     </template>
     <template #action="{ row }">
       <vxe-button type="text" status="primary" @click="insertRow(row, 'current')">插入节点</vxe-button>
@@ -289,3 +353,23 @@ const removeRow = async (row: any) => {
     </template>
   </vxe-grid>
 </template>
+
+<style>
+/* 默认单元格设置 */
+/* 解决 cell 的上下左右边距过宽的问题 */
+.vxe-table--render-default .vxe-cell {
+  padding: 0 0;
+}
+
+/* 解决 tree 树形表格的单元格 edit 状态无法撑满 td 的问题 */
+.vxe-table .vxe-body--column.no-indent .vxe-cell--tree-node {
+  padding-left: 0!important;
+}
+.vxe-table .vxe-body--column.no-indent .vxe-tree-cell {
+  padding-left: 0!important;
+}
+.vxe-table .vxe-body--column.no-indent .vxe-tree--btn-wrapper {
+  display: none;
+}
+
+</style>
